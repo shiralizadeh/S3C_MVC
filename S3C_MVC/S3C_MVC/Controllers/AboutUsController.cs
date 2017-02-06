a1cf021eb7329afb7bd26430aee2e0764a06b90a
samen05 <samen05@DESKTOP-FNDJ8Q4>
2017-02-06 19:42:05 +0330

    جلسه ی دوم

---
+++ S3C_MVC/S3C_MVC/Controllers/AboutUsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace S3C_MVC.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        // GET: AboutUs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ S3C_MVC/S3C_MVC/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace S3C_MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
