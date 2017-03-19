823cf0f768303ac55298056c22f723f5620df88f
Mohamad Shiralizadeh <mohamad.shir@live.com>
2017-03-19 11:59:42 +0330

    Remove DI

--- S3C_MVC/S3C_MVC/Global.asax.cs
+++ S3C_MVC/S3C_MVC/Global.asax.cs
@@ -2,7 +2,6 @@
 using S3C_MVC.DataLayer;
 using S3C_MVC.Models.Admin;
 using S3C_MVC.Services;
-using StructureMap;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,53 +33,6 @@ namespace S3C_MVC
                 config.CreateMap<Models.Public.ProductDTO, Product>();
                 config.CreateMap<Product, Models.Public.ProductDTO>();
             });
-
-            var container = new Container(config =>
-            {
-                //config.Scan(scan =>
-                //{
-                //    //scan.TheCallingAssembly();
-                //    scan.WithDefaultConventions();
-                //});
-
-                config.For<IProductsServices>().Use<ProductsServices>();
-            });
-
         }
     }
-
-    //public class StructureMapControllerFactory : DefaultControllerFactory
-    //{
-    //    protected override IController
-    //        GetControllerInstance(RequestContext requestContext,
-    //        Type controllerType)
-    //    {
-    //        try
-    //        {
-    //            if ((requestContext == null) || (controllerType == null))
-    //                return null;
-
-    //            return (Controller).GetInstance(controllerType);
-    //        }
-    //        catch (StructureMapException)
-    //        {
-    //            System.Diagnostics.Debug.WriteLine(ObjectFactory.WhatDoIHave());
-    //            throw new Exception(ObjectFactory.WhatDoIHave());
-    //        }
-    //    }
-    //}
-
-    //public static class Bootstrapper
-    //{
-    //    public static void Run()
-    //    {
-    //        ControllerBuilder.Current
-    //            .SetControllerFactory(new StructureMapControllerFactory());
-
-    //        ObjectFactory.Initialize(x =>
-    //        {
-
-    //        });
-    //    }
-    //}
 }
